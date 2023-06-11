b099e7ce3b77046dc060d2a7168ee0d370d73c49
Tri Aldy Kurniawan <122498697+Kenji2005@users.noreply.github.com>
2023-06-11 13:18:20 +0700

    Update DataSidebar.php

--- app/Traits/DataSidebar.php
+++ app/Traits/DataSidebar.php
@@ -15,23 +15,23 @@ trait DataSidebar
 {
     function AssignmentMockup()
     {
-        if (auth()->user()->roles->pluck('name')[0] == 'mentor' || auth()->user()->roles->pluck('name')[0] == 'teacher' || auth()->user()->roles->pluck('name')[0] == 'admin' || auth()->user()->roles->pluck('name')[0] == 'school') {
-            return [];
+        if(auth()->user()->roles->pluck('name')[0] == 'student'){
+            $generationId = Auth()->user()->studentSchool->studentClassroom->classroom->generation_id;
+            return Assignment::whereIn('sub_material_id', function ($query) use ($generationId) {
+                $query->select('id')
+                    ->from('sub_materials')
+                    ->whereIn('material_id', function ($query) use ($generationId) {
+                        $query->select('id')
+                            ->from('materials')
+                            ->whereIn('generation_id', function ($query) use ($generationId) {
+                                $query->select('id')
+                                    ->from('generations')
+                                    ->where('id', $generationId);
+                            });
+                    });
+            })->orderBy('end_date', 'desc')->take(5)->get();
         }
-        $generationId = Auth()->user()->studentSchool->studentClassroom->classroom->generation_id;
-        return Assignment::whereIn('sub_material_id', function ($query) use ($generationId) {
-            $query->select('id')
-                ->from('sub_materials')
-                ->whereIn('material_id', function ($query) use ($generationId) {
-                    $query->select('id')
-                        ->from('materials')
-                        ->whereIn('generation_id', function ($query) use ($generationId) {
-                            $query->select('id')
-                                ->from('generations')
-                                ->where('id', $generationId);
-                        });
-                });
-        })->orderBy('end_date', 'desc')->take(5)->get();
+        return [];
     }
 
 
@@ -53,11 +53,13 @@ trait DataSidebar
 
     function ChallengeMockup()
     {
-        if (auth()->user()->roles->pluck('name')[0] == 'mentor' || auth()->user()->roles->pluck('name')[0] == 'teacher' || auth()->user()->roles->pluck('name')[0] == 'admin' || auth()->user()->roles->pluck('name')[0] == 'school') {
-            return [];
+        if(auth()->user()->roles->pluck('name')[0] == 'student'){
+            $classroomId = Auth()->user()->studentSchool->studentClassroom->classroom->id;
+            return Challenge::where('classroom_id', $classroomId)->orderBy('end_date', 'desc')->take(5)->get();
         }
-        $classroomId = Auth()->user()->studentSchool->studentClassroom->classroom->id;
-        return Challenge::where('classroom_id', $classroomId)->orderBy('end_date', 'desc')->take(5)->get();
+
+        return [];
+        
     }
 
 
@@ -68,12 +70,13 @@ trait DataSidebar
 
     function ScheduleMockup()
     {
-        if (auth()->user()->roles->pluck('name')[0] == 'mentor') {
+        $role = auth()->user()->roles->pluck('name')[0];
+        if ($role == 'mentor') {
             return ZoomSchedule::where('mentor_id', Auth()->id())->where('date', '>', Carbon::now())->orderBy('date', 'desc')->take(5)->get();
-        } else if (auth()->user()->roles->pluck('name')[0] == 'student') {
+        } else if ($role == 'student') {
             $classroomId = Auth()->user()->studentSchool->studentClassroom->classroom->id;
             return ZoomSchedule::where('classroom_id', $classroomId)->where('date', '>', Carbon::now())->orderBy('date', 'desc')->take(5)->get();
-        } else if (auth()->user()->roles->pluck('name')[0] == 'teacher') {
+        } else if ($role == 'teacher') {
             $classroomId = Auth()->user()->teacherSchool->teacherClassrooms->pluck('classroom_id')->toArray();
             return ZoomSchedule::whereIn('classroom_id', $classroomId)->where('date', '>', Carbon::now())->orderBy('date', 'desc')->take(5)->get();
         }
