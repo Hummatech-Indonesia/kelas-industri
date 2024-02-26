8df6752d0f79bf15b22b5c689ae13c959b35b6cf
kearaaa <dekaraa15@gmail.com>
2024-02-26 23:21:04 +0700

    Create 2024_02_26_114123_add_generation_to_salary.php

---
+++ database/migrations/2024_02_26_114123_add_generation_to_salary.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('salaries', function (Blueprint $table) {
+            //
+            $table->foreignId('generation_id')->nullable()->constrained('generations')->after('user_id')->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('salaries', function (Blueprint $table) {
+            //
+            $table->dropForeign(['generation_id']);
+            $table->dropColumn('generation_id');
+        });
+    }
+};
