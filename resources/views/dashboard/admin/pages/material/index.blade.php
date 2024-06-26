acaf02fce8f208a23e9d0ea8a09744fc17115960
wyruxx <marufnurfauzan@gmail.com>
2024-06-26 15:16:14 +0700

    ganti warna button
    ganti warna button

--- resources/views/dashboard/admin/pages/material/index.blade.php
+++ resources/views/dashboard/admin/pages/material/index.blade.php
@@ -23,7 +23,7 @@
         <div class="d-flex align-items-center py-2 py-md-1">
 
             <!--begin::Button-->
-            <a href="{{ route('admin.materials.create') }}" class="btn btn-dark fw-bold">
+            <a href="{{ route('admin.materials.create') }}" class="btn btn-primary fw-bold">
                 Tambah </a>
             <!--end::Button-->
         </div>
--- resources/views/dashboard/admin/pages/zoomSchedule/index.blade.php
+++ resources/views/dashboard/admin/pages/zoomSchedule/index.blade.php
@@ -24,7 +24,7 @@
         <div class="d-flex align-items-center py-2 py-md-1">
 
             <!--begin::Button-->
-            <a href="{{ route('admin.zoomSchedules.create') }}" class="btn btn-dark fw-bold">
+            <a href="{{ route('admin.zoomSchedules.create') }}" class="btn btn-primary fw-bold">
                 Tambah </a>
             <!--end::Button-->
         </div>
