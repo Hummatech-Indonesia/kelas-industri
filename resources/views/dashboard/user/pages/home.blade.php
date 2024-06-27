9af2e3d915a56333a78ade73c4ae2556fd55ea3b
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-06-27 13:07:03 +0700

    Update home.blade.php

--- resources/views/dashboard/user/pages/home.blade.php
+++ resources/views/dashboard/user/pages/home.blade.php
@@ -242,11 +242,7 @@
                             <a href="#" class="card hover-elevate-up col col-lg-3 shadow-sm parent-hover">
                                 <div class="card-body d-flex align-items">
                                     <span class="w-4 h-4 my-auto fs-1">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
-                                            viewBox="0 0 24 24">
-                                            <path fill="currentColor"
-                                                d="m3 14l.5.07L8.07 9.5a1.95 1.95 0 0 1 .52-1.91c.78-.79 2.04-.79 2.82 0c.53.52.7 1.26.52 1.91l2.57 2.57l.5-.07c.18 0 .35 0 .5.07l3.57-3.57C19 8.35 19 8.18 19 8a2 2 0 0 1 2-2a2 2 0 0 1 2 2a2 2 0 0 1-2 2c-.18 0-.35 0-.5-.07l-3.57 3.57c.07.15.07.32.07.5a2 2 0 0 1-2 2a2 2 0 0 1-2-2l.07-.5l-2.57-2.57c-.32.07-.68.07-1 0L4.93 15.5L5 16a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2Z" />
-                                        </svg>
+                                        <i class="bi bi-megaphone-fill fs-1 text-primary"></i>
                                     </span>
 
                                     <span class="ms-3 text-gray-700 parent-hover-primary fs-6 fw-bold my-auto">
