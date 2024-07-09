1bf4da5fcea00a385e2be8436d6ac0965aafe0ba
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-07-09 15:45:48 +0700

    Update navbar.blade.php

--- resources/views/dashboard/exam/layouts/navbar.blade.php
+++ resources/views/dashboard/exam/layouts/navbar.blade.php
@@ -297,6 +297,20 @@
                                 </form>
                             </div>
                             <!--end::Menu item-->
+                        @elseif($exam != null && $exam->score != null)
+                            <!--begin::Menu item-->
+                            <div class="menu-item px-5">
+                                <a href="{{ route('logout') }}" class="menu-link px-5"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    Keluar
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                            <!--end::Menu item-->
                         @endif
                     </div>
                     <!--end::User account menu-->
