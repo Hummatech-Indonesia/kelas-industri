8d6dde2c87ee02cca6b967807e5b2756f237297f
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-07-31 14:17:32 +0700

    Update CertifyService.php

--- app/Services/CertifyService.php
+++ app/Services/CertifyService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Enums\MaterialExamTypeEnum;
 use App\Models\Material;
 
 class CertifyService
@@ -16,64 +17,50 @@ class CertifyService
             'subMaterials.assignments.StudentSubmitAssignment' => function ($query) {
                 $query->where('student_id', auth()->user()->id)
                     ->select('assignment_id', 'point');
-            }
-        ])->where('generation_id', $classroom->generation_id)
-            ->get();
+            },
+        ])
+        ->with('materialExam.studentMaterialExams', function($query) {
+            $query->where('student_id', auth()->user()->id)
+            ->where('type', MaterialExamTypeEnum::POSTEST->value);
+        })
+        ->where('generation_id', $classroom->generation_id)
+        ->get();
 
         $data = $studentScores->map(function ($material) {
-
-            $totalAssignmentCount = $material->subMaterials->sum(function ($subMaterial) {
-                return $subMaterial->assignments->count();
-            });
-
-            $totalExamCount = $material->subMaterials->sum(function ($subMaterial) {
-                return $subMaterial->subMaterialExams->count();
-            });
+            $totalAssignmentCount = $material->subMaterials->sum(fn($subMaterial) => $subMaterial->assignments->count());
+            $totalExamCount = $material->subMaterials->sum(fn($subMaterial) => $subMaterial->subMaterialExams->count());
+            $totalMaterialExamCount = $material->materialExam->studentMaterialExams->count();
 
             $subMaterialScores = $material->subMaterials->flatMap(function ($subMaterial) {
                 return $subMaterial->subMaterialExams->flatMap(function ($subMaterialExam) {
-                    return $subMaterialExam->studentSubmaterialExams->map(function ($studentSubmaterialExam) {
-                        return [
-                            'sub_material_exam_id' => $studentSubmaterialExam->sub_material_exam_id,
-                            'score' => $studentSubmaterialExam->score,
-                            'sub_material_id' => $studentSubmaterialExam->subMaterialExam->sub_material_id,
-                        ];
-                    });
+                    return $subMaterialExam->studentSubmaterialExams->map(fn($studentSubmaterialExam) => [
+                        'sub_material_exam_id' => $studentSubmaterialExam->sub_material_exam_id,
+                        'score' => $studentSubmaterialExam->score,
+                        'sub_material_id' => $studentSubmaterialExam->subMaterialExam->sub_material_id,
+                    ]);
                 });
-            })->groupBy('sub_material_id')
-                ->map(function ($scores) {
-                    return [
-                        'total_score' => $scores->sum('score'),
-                    ];
-                });
+            })->groupBy('sub_material_id')->map(fn($scores) => [
+                'total_score' => $scores->sum('score'),
+            ]);
 
             $assignmentScores = $material->subMaterials->flatMap(function ($subMaterial) {
                 return $subMaterial->assignments->flatMap(function ($assignment) {
-                    return $assignment->StudentSubmitAssignment->map(function ($studentAssignment) {
-                        return [
-                            'assignment_id' => $studentAssignment->assignment_id,
-                            'point' => $studentAssignment->point,
-                        ];
-                    });
+                    return $assignment->StudentSubmitAssignment->map(fn($studentAssignment) => [
+                        'assignment_id' => $studentAssignment->assignment_id,
+                        'point' => $studentAssignment->point,
+                    ]);
                 });
-            })->groupBy('assignment_id')
-                ->map(function ($scores) {
-                    return [
-                        'total_score' => $scores->sum('point'),
-                    ];
-                });
+            })->groupBy('assignment_id')->map(fn($scores) => [
+                'total_score' => $scores->sum('point'),
+            ]);
 
-            $totalExamScore = $subMaterialScores->reduce(function ($carry, $item) {
-                return $carry + $item['total_score'];
-            }, 0);
+            $totalExamScore = $subMaterialScores->sum('total_score');
+            $totalAssignmentScore = $assignmentScores->sum('total_score');
 
-            $totalAssignmentScore = $assignmentScores->reduce(function ($carry, $item) {
-                return $carry + $item['total_score'];
-            }, 0);
+            $postTestScore = $material->materialExam->studentMaterialExams->sum('score');
 
-            $assignmentScore = $totalAssignmentScore / $totalAssignmentCount;
-
-            $examScore = $totalExamScore / $totalExamCount;
+            $assignmentScore = $totalAssignmentScore / max($totalAssignmentCount, 1);
+            $examScore = $postTestScore / max($totalMaterialExamCount, 1);
 
             $totalScore = ($examScore * 0.6) + ($assignmentScore * 0.4);
 
