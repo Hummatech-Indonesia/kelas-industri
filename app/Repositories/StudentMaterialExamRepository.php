aea52c87f513e7584002c13cada2a0f84536d08f
mohamadarif03 <pecintarealmadrid2@gmail.com>
2024-08-11 11:28:07 +0700

    Update StudentMaterialExamRepository.php

--- app/Repositories/StudentMaterialExamRepository.php
+++ app/Repositories/StudentMaterialExamRepository.php
@@ -88,7 +88,7 @@ class StudentMaterialExamRepository extends BaseRepository
 
     public function handleComplateExamPreTest(mixed $previousMaterial): mixed
     {
-        dd('test');
+        dd($previousMaterial);
         return $this->model->query()
             ->where('material_exam_id', $previousMaterial->materialExam->id)
             ->whereRelation('student', 'id', auth()->user()->id)
