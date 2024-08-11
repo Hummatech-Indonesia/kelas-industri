bcc0fdd404114e9b486c5501fc55cb53950c7052
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-08-11 13:43:47 +0700

    Update UserClassroomController.php

--- app/Http/Controllers/UserClassroomController.php
+++ app/Http/Controllers/UserClassroomController.php
@@ -100,11 +100,11 @@ class UserClassroomController extends Controller
         $data = $this->GetDataSidebar();
         $data['classroom'] = $classroom;
         $data['materials'] = $this->materialService->handleByClassroom($classroom, $request);
+        dd($data['materials']);
         if (auth()->user()->roles->pluck('name')[0] == 'student') {
             $data['materialInfos'] = $this->materialService->handleOrderMaterials($data['materials']);
         }
         $data['search'] = $request->search;
-        // dd($data);
         return view('dashboard.user.pages.material.index', $data);
     }
 
