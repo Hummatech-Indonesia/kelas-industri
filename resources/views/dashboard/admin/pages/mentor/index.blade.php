44508a1723d7f76afa4ed26b567fb54393149f49
mohamadarif03 <pecintarealmadrid2@gmail.com>
2024-08-12 13:07:21 +0700

    Update index.blade.php

--- resources/views/dashboard/admin/pages/mentor/index.blade.php
+++ resources/views/dashboard/admin/pages/mentor/index.blade.php
@@ -1,8 +1,7 @@
 @extends('dashboard.admin.layouts.app')
+
 @section('content')
     <div class="toolbar mb-5 mb-lg-7" id="kt_toolbar">
-
-
         <!--begin::Page title-->
         <div class="page-title d-flex flex-column me-3">
             <!--begin::Title-->
@@ -10,8 +9,6 @@
                 Mentor
             </h1>
             <!--end::Title-->
-
-
             <!--begin::Breadcrumb-->
             <p class="text-muted">
                 List mentor pada kelas industri.
@@ -19,13 +16,12 @@
             <!--end::Breadcrumb-->
         </div>
         <!--end::Page title-->
-
         <!--begin::Actions-->
         <div class="d-flex align-items-center py-2 py-md-1">
-
             <!--begin::Button-->
             <a href="{{ route('admin.mentors.create') }}" class="btn btn-primary fw-bold">
-                Tambah </a>
+                Tambah
+            </a>
             <!--end::Button-->
         </div>
         <!--end::Actions-->
@@ -34,63 +30,57 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
-
                     <!--begin::Card body-->
                     <div class="card-body pt-0">
-
                         <!--begin::Table-->
-                        <table class="table align-middle table-row-dashed fs-6 gy-5" id="datatables-responsive">
-                            <!--begin::Table head-->
-                            <thead>
-                            <!--begin::Table row-->
-                            <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
-                                <th>No</th>
-                                <th>Nama</th>
-                                <th>Email</th>
-                                <th>No Telepon</th>
-                                <th>No Rekening</th>
-                                <th>Bank</th>
-                                <th>Aksi</th>
-                            </tr>
-                            <!--end::Table row-->
-                            </thead>
-                            <!--end::Table head-->
-
-                            <!--begin::Table body-->
-                            <tbody class="fw-semibold text-gray-600">
-                            </tbody>
-                            <!--end::Table body-->
-                        </table>
+                        <div class="table-responsive">
+                            <table class="table align-middle table-row-dashed fs-6 gy-5" id="datatables-responsive">
+                                <!--begin::Table head-->
+                                <thead>
+                                    <!--begin::Table row-->
+                                    <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>Email</th>
+                                        <th>No Telepon</th>
+                                        <th>No Rekening</th>
+                                        <th>Bank</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                    <!--end::Table row-->
+                                </thead>
+                                <!--end::Table head-->
+                                <!--begin::Table body-->
+                                <tbody class="fw-semibold text-gray-600">
+                                </tbody>
+                                <!--end::Table body-->
+                            </table>
+                        </div>
                         <!--end::Table-->
                     </div>
                     <!--end::Card body-->
                 </div>
             </div>
-
         </div>
-        <x-delete-modal-component/>
+        <x-delete-modal-component />
     </div>
 @endsection
+
 @section('script')
-    {{--    <script src="{{ asset('app-assets/js/custom/apps/customers/list/export.js') }}"></script>--}}
     <script src="{{ asset('app-assets/js/custom/apps/customers/list/list.js') }}"></script>
-    {{--    <script src="{{ asset('app-assets/js/custom/apps/customers/add.js') }}"></script>--}}
+    <script src="{{ asset('app-assets/js/datatables.min.js') }}"></script>
+    <script src="{{ asset('app-assets/js/datatables.responsive.min.js') }}"></script>
     <script>
         document.addEventListener("DOMContentLoaded", () => {
+            $(document).on('click', '.delete', function() {
+                const url = "{{ route('admin.mentors.destroy', ':id') }}".replace(':id', $(this).data(
+                'id'));
+                $('#form-delete').attr('action', url);
+                $('#kt_modal_delete').modal('show');
+            });
 
-            $(document).on('click', '.delete', function () {
-                const url = "{{ route('admin.mentors.destroy', ':id') }}".replace(':id', $(this).data('id'))
-                $('#form-delete').attr('action', url)
-
-                $('#kt_modal_delete').modal('show')
-            })
-
-            // Datatables Responsive
+            // Initialize DataTables
             $("#datatables-responsive").DataTable({
-                // scrollX: false,
-                // sScrollX: false,
-                // scrollY: false,
-                // scrollCollapse: false,
                 paging: true,
                 pageLength: 10,
                 responsive: true,
@@ -98,9 +88,10 @@
                 serverSide: true,
                 searching: true,
                 ajax: "{{ route('admin.mentors.index') }}",
-                oLanguage: {sProcessing: 'loading...'},
-                columns: [
-                    {
+                oLanguage: {
+                    sProcessing: 'loading...'
+                },
+                columns: [{
                         data: 'DT_RowIndex',
                         searchable: false
                     },
