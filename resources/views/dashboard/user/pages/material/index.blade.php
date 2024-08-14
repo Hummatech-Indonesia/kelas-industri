3d067050f758fb98b97221ca16d86556f31ad0cd
mohamadarif03 <pecintarealmadrid2@gmail.com>
2024-08-14 10:41:42 +0700

    push

--- resources/views/dashboard/user/pages/material/index.blade.php
+++ resources/views/dashboard/user/pages/material/index.blade.php
@@ -461,10 +461,11 @@
                                                                 </svg>
                                                             </span>
                                                             <!--end::Svg Icon-->
-                                                            <a href="{{ route('common.showMaterial', ['classroom' => $classroom->id, 'material' => $material->id]) }}"
-                                                                class="fw-bold text-info ml-2">{{ count($material->subMaterials) }}
-                                                                Bab</a>
-
+                                                            @if ($prevMaterialComplete || !$currentMaterialPretestComplete)
+                                                                <a href="{{ route('common.showMaterial', ['classroom' => $classroom->id, 'material' => $material->id]) }}"
+                                                                    class="fw-bold text-info ml-2">{{ count($material->subMaterials) }}
+                                                                    Bab</a>
+                                                            @endif
 
                                                         </div>
 
