f8b5c92f4fc87d3b700fa042178c0ad342a3ada8
Wira29 <59402371+wira29@users.noreply.github.com>
2024-08-15 13:53:12 +0700

    mod: testing create zoom

--- app/Http/Controllers/ZoomScheduleController.php
+++ app/Http/Controllers/ZoomScheduleController.php
@@ -45,7 +45,7 @@ class ZoomScheduleController extends Controller
      */
     public function create(): mixed
     {
-        dd(csrf_token());
+        // dd(csrf_token());
         $currentSchoolYear = SchoolYearHelper::get_current_school_year();
         if (request()->ajax()) return $this->classroomService->handleGetBySchool(request()->school_id, $currentSchoolYear->id);
 
