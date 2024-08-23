d4fa446ae8cc89645d32f8b5469780b199e8fe7d
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-08-23 13:34:22 +0700

    Update ClassroomRepository.php

--- app/Repositories/ClassroomRepository.php
+++ app/Repositories/ClassroomRepository.php
@@ -242,12 +242,16 @@ class ClassroomRepository extends BaseRepository
     public function get_student_by_classroom(string $classroomId)
     {
         return $this->studentClassroom->query()
+            ->whereHas('studentSchool.student', function ($q) {
+                $q->where('status', 'active');
+            })
             ->where('classroom_id', $classroomId)
             ->get();
     }
 
 
-    public function first() : mixed {
+    public function first(): mixed
+    {
         return $this->model->query()->orderBy("name", "DESC")->first();
     }
 }
