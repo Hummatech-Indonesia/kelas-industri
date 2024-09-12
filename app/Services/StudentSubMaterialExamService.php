e9dec42768f906d440db0b32d47c05471737cd5c
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-09-12 13:27:03 +0700

    Update StudentSubMaterialExamService.php

--- app/Services/StudentSubMaterialExamService.php
+++ app/Services/StudentSubMaterialExamService.php
@@ -19,9 +19,7 @@ class StudentSubMaterialExamService
     {
         $studentExam = $this->repository->get_user_submaterial_exam($submaterialExamId);
 
-        if ($studentExam->finished_count >= 3) {
-            return 'limit';
-        } elseif ($studentExam->score < 75) {
+        if ($studentExam->score < 75) {
             return 'remedial';
         } else {
             return 'passed';
